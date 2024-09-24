1dc713f5abc3ca91cfe45dac9685c8139e4d2cc6
CorreaJo <j.victor.augusto2@gmail.com>
2024-09-23 23:38:46 -0300

    crud empresa

--- database.sql
+++ database.sql
@@ -15,5 +15,15 @@ CREATE TABLE Usuario (
     cidade VARCHAR (50) NOT NULL,
     linkedin VARCHAR (100) NULL,
     dNasc VARCHAR(8) NOT NULL
+);
 
+CREATE TABLE Empresa (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(100) NOT NULL,
+    cnpj VARCHAR(14) NOT NULL,
+    telefone VARCHAR(25) NOT NULL,
+    cidade VARCHAR (50) NOT NULL,
+    linkedin VARCHAR (100) NULL,
 );
