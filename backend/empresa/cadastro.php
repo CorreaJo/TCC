444a7b9ca8758c17244dc362d7693cd8f7f45833
CorreaJo <j.victor.augusto2@gmail.com>
2024-11-26 00:16:52 -0300

    Imagem Empresa

--- backend/empresa/cadastro.php
+++ backend/empresa/cadastro.php
@@ -12,8 +12,20 @@ $categoria = $_POST["categoria"];
 
 $hashSenha =  base64_encode($_POST["senha"]);
 
+$dir = "uploads/"; 
+// recebendo o arquivo multipart 
+$img = $_FILES["img"];
+// Move o arquivo da pasta temporaria de upload para a pasta de destino 
 
-$sql= "INSERT INTO empresa (nome, senha, email, cnpj, telefone, cidade, linkedin, categoria) VALUE ('$nome', '$hashSenha', '$email', '$cnpj', '$telefone', '$cidade', '$linkedin', '$categoria')";
+$nomeImg = "$dir".$img["name"];
+if (move_uploaded_file($img["tmp_name"], $nomeImg)) { 
+    echo "Arquivo enviado com sucesso!"; 
+} 
+else { 
+    echo "Erro"; 
+}
+
+$sql= "INSERT INTO empresa (nome, img, senha, email, cnpj, telefone, cidade, linkedin, categoria) VALUE ('$nome', '$nomeImg', '$hashSenha', '$email', '$cnpj', '$telefone', '$cidade', '$linkedin', '$categoria')";
 $result= mysqli_query($conexao, $sql);
 
 if($result) {
