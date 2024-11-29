823787e18b4c70d56cb2deddc074929be58b45f8
CorreaJo <j.victor.augusto2@gmail.com>
2024-11-29 17:53:00 -0300

    conexão banco de dados

--- backend/conexao.php
+++ backend/conexao.php
@@ -1,7 +1,9 @@
 <?php
 
+// hospedagem - mysql -hjunction.proxy.rlwy.net -uroot -pZddpHMlcQLwfNZVGQxZZkaFZZfqpttjO --port 44901 --protocol=TCP railway
+$conexaoHosp = mysqli_connect("mysql.railway.internal", "root", "ZddpHMlcQLwfNZVGQxZZkaFZZfqpttjO", "cordwork");
 $conexao = mysqli_connect("localhost", "root", "", "cordwork");
-if(!$conexao) {
+if(!$conexaoHosp) {
     echo "Erro de conexão com o banco de dados!";
     echo mysqli_connect_error();
 }
